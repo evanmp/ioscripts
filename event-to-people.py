e67305fd8a7a1749284d07e561e70dbf573a821f
Marshall Louis Reaves <marshall@mixpanel.com>
2013-11-23 14:09:43 -0800

    testing commit

--- event-to-people.py
+++ event-to-people.py
@@ -1,5 +1,16 @@
 '''
 
+step #1 get all events of a name
+1B get event name from sys args
 
+step #2 get all distinct ids from these events with the max date
+
+Step #3 get all people profiles -> text file
+
+Step #4 read into memory the text file of people profiles
+
+Step #5 create a list of intersecting distinct ids between events and people profiles
+
+Step #6 for each distinct_id, $set( EVENT_NAME: date ) with $ignore_time = TRUE
 
 '''
